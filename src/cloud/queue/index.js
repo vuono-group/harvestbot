b45f13e1f942da2d63a2b8f4666de5f9d547549e
Laura Vuorenoja <laura.vuorenoja@newthings.co>
2019-02-23 13:29:47 +0200

    Fix pubsub publishing.

--- src/cloud/queue/index.js
+++ src/cloud/queue/index.js
@@ -1,19 +1,19 @@
-import PubSub from '@google-cloud/pubsub';
+import { PubSub } from '@google-cloud/pubsub';
 
 export default (config) => {
   const topics = {
     flextime: 'flextime',
     stats: 'stats',
   };
-  const pubsubClient = PubSub({
+  const pubsubClient = new PubSub({
     projectId: config.projectId,
   });
 
   const enqueueFlexTimeRequest = data => pubsubClient
-    .topic(topics.flextime).publisher().publish(Buffer.from(JSON.stringify(data)));
+    .topic(topics.flextime).publish(Buffer.from(JSON.stringify(data)));
 
   const enqueueStatsRequest = data => pubsubClient
-    .topic(topics.stats).publisher().publish(Buffer.from(JSON.stringify(data)));
+    .topic(topics.stats).publish(Buffer.from(JSON.stringify(data)));
 
   return { enqueueFlexTimeRequest, enqueueStatsRequest };
 };
