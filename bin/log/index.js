9d9cadc440ec1641a824a9d378891e156b911b07
CircleCI <circleci@harvestbot>
2019-02-23 10:55:52 +0000

    [skip ci] Update deployable build output.

--- bin/log/index.js
+++ bin/log/index.js
@@ -23,7 +23,7 @@ var _default = ({
   };
   const loggingConfig = {
     format: _winston.format.combine(_winston.format.colorize(), _winston.format.timestamp(), _winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)),
-    level: 'INFO',
+    level: inGoogleCloud ? 'INFO' : 'info',
     transports: appTransports.default,
     exceptionHandlers: exceptionHandlers.default,
     exitOnError: true
