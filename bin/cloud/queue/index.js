00ec9faac88c8371e7fa9b0e0f09ed455dc24932
CircleCI <circleci@harvestbot>
2019-02-23 11:30:43 +0000

    [skip ci] Update deployable build output.

--- bin/cloud/queue/index.js
+++ bin/cloud/queue/index.js
@@ -1,26 +1,24 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
 
-var _pubsub = _interopRequireDefault(require("@google-cloud/pubsub"));
+var _pubsub = require("@google-cloud/pubsub");
 
 var _default = config => {
   const topics = {
     flextime: 'flextime',
     stats: 'stats'
   };
-  const pubsubClient = (0, _pubsub.default)({
+  const pubsubClient = new _pubsub.PubSub({
     projectId: config.projectId
   });
 
-  const enqueueFlexTimeRequest = data => pubsubClient.topic(topics.flextime).publisher().publish(Buffer.from(JSON.stringify(data)));
+  const enqueueFlexTimeRequest = data => pubsubClient.topic(topics.flextime).publish(Buffer.from(JSON.stringify(data)));
 
-  const enqueueStatsRequest = data => pubsubClient.topic(topics.stats).publisher().publish(Buffer.from(JSON.stringify(data)));
+  const enqueueStatsRequest = data => pubsubClient.topic(topics.stats).publish(Buffer.from(JSON.stringify(data)));
 
   return {
     enqueueFlexTimeRequest,
