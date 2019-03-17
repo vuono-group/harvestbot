67f5f737f548ae12af8784fdff2e961f4c075c11
CircleCI <circleci@harvestbot>
2019-03-17 07:57:20 +0000

    [skip ci] Update deployable build output.

--- bin/log/index.js
+++ bin/log/index.js
@@ -1,9 +1,13 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+
+var _objectSpread2 = _interopRequireDefault(require("@babel/runtime/helpers/objectSpread"));
 
 var _winston = require("winston");
 
@@ -19,15 +23,16 @@ var _default = ({
     default: [...(inGoogleCloud ? [new _loggingWinston.LoggingWinston()] : []), new Console()]
   };
   const exceptionHandlers = {
-    default: [new Console()]
+    default: [...(inGoogleCloud ? [new _loggingWinston.LoggingWinston()] : []), new Console()]
   };
-  const loggingConfig = {
-    format: _winston.format.combine(_winston.format.colorize(), _winston.format.timestamp(), _winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)),
+  const loggingConfig = (0, _objectSpread2.default)({}, inGoogleCloud ? {} : {
+    format: _winston.format.combine(_winston.format.colorize(), _winston.format.timestamp(), _winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`))
+  }, {
     level: 'info',
     transports: appTransports.default,
     exceptionHandlers: exceptionHandlers.default,
     exitOnError: true
-  };
+  });
   const logger = (0, _winston.createLogger)(loggingConfig);
   return logger;
 };
