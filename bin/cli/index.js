03034562f1a38aa529230e294646f3470595a01c
CircleCI <circleci@harvestbot>
2019-03-17 11:07:42 +0000

    [skip ci] Update deployable build output.

--- bin/cli/index.js
+++ bin/cli/index.js
@@ -26,7 +26,8 @@ var _default = (config, http) => {
   const logger = (0, _log.default)(config);
   const app = (0, _app.default)(config, http);
   const {
-    encryptSecret
+    encryptSecret,
+    decryptSecret
   } = (0, _keyRing.default)(config);
 
   const printResponse = (header, msgs) => {
@@ -53,6 +54,26 @@ var _default = (config, http) => {
     encryptSecret(JSON.stringify(_config.default));
   };
 
+  const decryptConfiguration = async () => {
+    const conf = JSON.parse((await decryptSecret()));
+    /* eslint-disable no-console */
+
+    console.log(`export ALLOWED_EMAIL_DOMAINS=${conf.emailDomains}`);
+    console.log(`export HARVEST_ACCESS_TOKEN=${conf.harvestAccessToken}`);
+    console.log(`export HARVEST_ACCOUNT_ID=${conf.harvestAccountId}`);
+    console.log(`export SLACK_BOT_TOKEN=${conf.slackBotToken}`);
+    console.log(`export SLACK_SIGNING_SECRET=${conf.slackSigningSecret}`);
+    console.log(`export SLACK_NOTIFY_CHANNEL_ID=${conf.notifyChannelId}`);
+    console.log(`export HOURS_STATS_COLUMN_HEADERS=${conf.hoursStatsColumnHeaders}`);
+    console.log(`export SENDGRID_API_KEY=${conf.sendGridApiKey}`);
+    console.log(`export TASK_ID_PUBLIC_HOLIDAY=${conf.taskIds.publicHoliday}`);
+    console.log(`export TASK_ID_VACATION=${conf.taskIds.vacation}`);
+    console.log(`export TASK_ID_UNPAID_LEAVE=${conf.taskIds.unpaidLeave}`);
+    console.log(`export TASK_ID_SICK_LEAVE=${conf.taskIds.sickLeave}`);
+    console.log(`export TASK_ID_FLEX_LEAVE=${conf.taskIds.flexLeave}`);
+    /* eslint-enable no-console */
+  };
+
   const start = () => {
     _commander.default.version(_package.version, '-v, --version');
 
@@ -61,6 +82,8 @@ var _default = (config, http) => {
     _commander.default.command('flextime <email>').description('Calculate flex saldo for given user.').action(calcFlexTime);
 
     _commander.default.command('encrypt').description('Encrypt and store app configuration.').action(encryptConfiguration);
+
+    _commander.default.command('decrypt').description('Decrypt and show app configuration.').action(decryptConfiguration);
 
     _commander.default.parse(process.argv);
   };
